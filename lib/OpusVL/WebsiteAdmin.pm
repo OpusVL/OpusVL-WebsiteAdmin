1d5770087bfa041a073fc7efbb6d2697670577ff
Colin Newell <colin@opusvl.com>
2013-05-21 12:08:23 +0100

    Bumped version number

--- lib/OpusVL/WebsiteAdmin.pm
+++ lib/OpusVL/WebsiteAdmin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use OpusVL::WebsiteAdmin::Builder;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 my $builder = OpusVL::WebsiteAdmin::Builder->new(
     appname => __PACKAGE__,
